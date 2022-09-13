e0bf88c6d4bdc23fa74aedb400424ab34c2ae850
Jen-Chieh Shen <jcs090218@gmail.com>
2022-09-13 15:54:03 +0800

    feat(gen): build once

--- scripts/generate-api.el
+++ scripts/generate-api.el
@@ -17,6 +17,13 @@
   "Kill current line."
   (goto-char (line-beginning-position)) (kill-line 1))
 
+(defun let-kill-scope ()
+  "Kill a scope."
+  (interactive)
+  (let ((beg (point)))
+    (forward-sexp)
+    (kill-region beg (point))))
+
 (with-current-buffer (find-file (locate-project-file "eask-api.el"))
   (erase-buffer)
   (insert-file-contents (locate-project-file "templates/source.el"))
@@ -27,9 +34,30 @@
   (let-kill-line)
 
   (dolist (path (directory-files-recursively eask-lisp-path "[.]el$"))
-
-    (message "path: %s" path)
-    )
+    (insert "\n")
+    (insert ";; " path)
+    (insert "\n")
+    (insert
+     (with-temp-buffer
+       (insert-file-contents path)
+       (goto-char (point-min))
+       (let ((line (thing-at-point 'line)))
+         (cond ((or (string-prefix-p ";;" line)
+                    (string-empty-p (string-trim line)))
+                (let-kill-line))
+               ;; skip these
+               ((or (string-prefix-p "(defun ")
+                    (string-prefix-p "(defcustom ")
+                    (string-prefix-p "(defvar ")
+                    (string-prefix-p "(defmacro "))
+                (forward-sexp)
+                (forward-line 1)
+                (goto-char (line-beginning-position)))
+               ;; other scope, we kill the entire scope
+               ((string-prefix-p "(") (let-kill-scope))
+               ;; kill a line for everything else
+               (t (let-kill-line))))
+       (buffer-string))))
 
   (message (buffer-string))
 
