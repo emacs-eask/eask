f948d7c22f63e203cc8a1730be9bc4d269a9878e
Jen-Chieh Shen <jcs090218@gmail.com>
2022-09-13 12:46:05 +0800

    feat(generate): wip

--- scripts/generate-api.el
+++ scripts/generate-api.el
@@ -2,6 +2,10 @@
 ;;; Commentary:
 ;;; Code:
 
+(defconst eask-lisp-path "~/lisp/"
+  "Eask's lisp directory.")
 
+(dolist (path (directory-files-recursively eask-lisp-path "[.]el$"))
+  (message "path: %s" path))
 
 ;;; generate-api.el ends here
