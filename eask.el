8383245ab952eb9c2579d4efb1e1020a89e571e1
Jen-Chieh <jcs090218@gmail.com>
2023-01-30 05:08:47 +0800

    fix: Rename package to eask

---
+++ eask.el
@@ -0,0 +1,37 @@
+;;; eask.el ---   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Shen, Jen-Chieh
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; URL: https://github.com/emacs-eask/eask-api
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1"))
+;; Keywords: lisp eask api
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Entry file to preempt the name `eask'
+;;
+
+;;; Code:
+
+(require 'eask-api)
+
+(provide 'eask)
+;;; eask.el ends here
