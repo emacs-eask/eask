ac39338969befad94c2dcfb8375232497b1e81a7
JenChieh <jcs090218@gmail.com>
2023-03-13 03:35:45 -0700

    correct order

--- scripts/generate-source.el
+++ scripts/generate-source.el
@@ -19,35 +19,40 @@
   (search-forward "{ SOURCE }")
   (let-kill-line)
 
-  (dolist (path (directory-files-recursively eask-lisp-path "[.]el$"))
-    (message "Processing file %s..." path)
-    (insert "\n")
-    (insert ";; " path)
-    (insert "\n")
-    (insert
-     (with-temp-buffer
-       (insert-file-contents path)
-       (goto-char (point-min))
-       (while (not (eobp))
-         (let ((line (thing-at-point 'line)))
-           (cond ((or (string-prefix-p ";;" line)
-                      (string-empty-p (string-trim line)))
-                  (let-kill-line))
-                 ;; skip these
-                 ((or (string-prefix-p "(defclass " line)
-                      (string-prefix-p "(defconst " line)
-                      (string-prefix-p "(defcustom " line)
-                      (string-prefix-p "(defun " line)
-                      (string-prefix-p "(defmacro " line)
-                      (string-prefix-p "(defvar " line))
-                  (forward-sexp)
-                  (forward-line 1)
-                  (goto-char (line-beginning-position)))
-                 ;; other scope, we kill the entire scope
-                 ((string-prefix-p "(" line) (let-kill-scope))
-                 ;; kill a line for everything else
-                 (t (let-kill-line)))))
-       (buffer-string))))
+  (let* ((files (directory-files-recursively eask-lisp-path "[.]el$"))
+         (files (sort files
+                      (lambda (file1 &rest _)
+                        ;; Sort `_prepare.el' to the top!
+                        (string-prefix-p "_" (file-name-nondirectory file1))))))
+    (dolist (path files)
+      (message "Processing file %s..." path)
+      (insert "\n")
+      (insert ";; " path)
+      (insert "\n")
+      (insert
+       (with-temp-buffer
+         (insert-file-contents path)
+         (goto-char (point-min))
+         (while (not (eobp))
+           (let ((line (thing-at-point 'line)))
+             (cond ((or (string-prefix-p ";;" line)
+                        (string-empty-p (string-trim line)))
+                    (let-kill-line))
+                   ;; skip these
+                   ((or (string-prefix-p "(defclass " line)
+                        (string-prefix-p "(defconst " line)
+                        (string-prefix-p "(defcustom " line)
+                        (string-prefix-p "(defun " line)
+                        (string-prefix-p "(defmacro " line)
+                        (string-prefix-p "(defvar " line))
+                    (forward-sexp)
+                    (forward-line 1)
+                    (goto-char (line-beginning-position)))
+                   ;; other scope, we kill the entire scope
+                   ((string-prefix-p "(" line) (let-kill-scope))
+                   ;; kill a line for everything else
+                   (t (let-kill-line)))))
+         (buffer-string)))))
 
   (save-buffer))
 
