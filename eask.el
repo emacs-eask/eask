3f8d4b668c4568d65f40c4f8bbb20c3b8dbfaa05
JenChieh <jcs090218@gmail.com>
2024-01-28 02:12:27 -0800

    Bump

--- eask.el
+++ eask.el
@@ -5,7 +5,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; URL: https://github.com/emacs-eask/eask
-;; Version: 0.9.3
+;; Version: 0.9.4
 ;; Package-Requires: ((emacs "26.1"))
 ;; Keywords: lisp eask api
 
