25f7f00ad4d73aacca027d6e1ba6070b8b2019b0
JenChieh <jcs090218@gmail.com>
2025-01-01 00:36:17 -0800

    Update copyright year

--- eask.el
+++ eask.el
@@ -1,6 +1,6 @@
 ;;; eask.el --- Core Eask APIs, for Eask CLI development  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023-2024  Shen, Jen-Chieh
+;; Copyright (C) 2023-2025  Shen, Jen-Chieh
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
