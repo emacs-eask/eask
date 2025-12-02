cbdd817b2cdd34963788ee996a3f6248deb842a5
Jen-Chieh Shen <jcs090218@gmail.com>
2025-12-02 22:11:48 +0800

    chore: Bump version for next release

--- eask.el
+++ eask.el
@@ -5,7 +5,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; URL: https://github.com/emacs-eask/eask
-;; Version: 0.12.0
+;; Version: 0.12.1
 ;; Package-Requires: ((emacs "26.1"))
 ;; Keywords: lisp eask api
 
