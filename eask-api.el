ec894011a91d300b64127905fe12660c1cd3ee4a
JenChieh <jcs090218@gmail.com>
2026-01-01 00:06:31 +0800

    chore: Update copyright info

--- eask-api.el
+++ eask-api.el
@@ -1,6 +1,6 @@
 ;;; eask-api.el --- Core Eask APIs  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022-2025  Shen, Jen-Chieh
+;; Copyright (C) 2022-2026  Shen, Jen-Chieh
 
 ;; This file is not part of GNU Emacs.
 
